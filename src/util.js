8fbf25bbeaaca1b42d4003002be7c16da74d5982
fellinga <mario.fellinger@outlook.com>
2017-06-20 14:44:07 +0200

    not needed anymore

--- src/util.js
+++ src/util.js
@@ -25,8 +25,21 @@ function viewBtn() {
     barViewBtn();           // function located at barChart.js
     forceViewBtn();         // function located at forceGraph.js
 }
-
 function deselect() {
     selectBar("");          // function located at barChart.js
     selectNode("");         // function located at forceGraph.js
+}
+
+// CHART INFORMATION BUTTON
+function info() {
+    alert("top 50 used tags on superuser.com (year 2016)\n\n" +
+
+          "Count: How often a tag was used. \n" +
+          "Comments: How many comments a tag got. \n" +
+          "Answers: How many answers a tag got. \n" +
+          "Favorites: How many favorites a tag got. \n" +
+          "Score: The score a tag got. \n" +
+          "Views: How many views a tag got. \n" +
+
+          "\nand how often they appear with each other.");
 }
